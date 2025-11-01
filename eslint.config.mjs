ac5b78e0da0948c09b0f9339ed2cf01292027dab
HaCaoVi <icaovy2001@gmail.com>
2025-11-01 23:33:15 +0700

    revert config elint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,14 +21,8 @@ const eslintConfig = [
     ],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
-      "@typescript-eslint/no-wrapper-object-types": "off",
       "react/no-unescaped-entities": "off",
-      "react-hooks/exhaustive-deps": "off",
       "prefer-const": "off",
-      "@next/next/no-img-element": "off",
-      "@next/next/no-html-link-for-pages": "off",
     },
   },
 ];
